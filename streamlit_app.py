9ac8942c7cde7927fef1df51002795ab5adb4707
akshithabapathu88-dot <akshithabapathu88@gmail.com>
2026-04-06 21:00:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import sys
 import os
 from datetime import datetime
 import json
-
+os.system("python simple_setup.py")
 # Add parent directory to path to import modules
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
